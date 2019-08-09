0376ab2db63fe487755338ebb938ca9ba87d3839
atsushi.inoue <atsushi.inoue@probizmo.co.jp>
2019-08-09 18:22:24 +0900

    support systemd

--- lib/itamae/plugin/recipe/god/install.rb
+++ lib/itamae/plugin/recipe/god/install.rb
@@ -17,11 +17,31 @@ template '/etc/god/master.conf' do
   mode '644'
 end
 
-template '/etc/init.d/god' do
-  user 'root'
-  owner 'root'
-  group 'root'
-  mode '755'
+service_variables = {
+  pid: '/var/run/god.pid',
+  config: '/etc/god/master.conf',
+  log: '/var/log/god.log',
+  log_level: 'info'
+}
+
+case "#{node.platform_family}-#{node.platform_version}"
+when /rhel-7\.(.*?)/
+  which_god = run_command('which god')
+  template '/etc/systemd/system/god.service' do
+    user 'root'
+    owner 'root'
+    group 'root'
+    mode '755'
+    variables service_variables.merge({command: which_god.stdout.chomp})
+  end
+else
+  template '/etc/init.d/god' do
+    user 'root'
+    owner 'root'
+    group 'root'
+    mode '755'
+    variables service_variables
+  end
 end
 
 template '/etc/logrotate.d/god' do
